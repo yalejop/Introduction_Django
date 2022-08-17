a0e036f84b773df028a437949cdc6d8d3143446a
Alejandro Cataño Herrera <alejo.9211@hotmail.com>
2022-08-16 23:02:47 -0500

    update a view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import View
+from django.views.generic import View, UpdateView
 from .forms import PostCreateForm
 from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
 class BlogListView(View):
@@ -34,8 +35,7 @@ class BlogCreateView(View):
             
         }
         return render(request, 'blog_create.html', context)
-    
-    
+
 class BlogDetailView(View):
     def get(self, request, pk,  *args, **kwargs):
         post = get_object_or_404(Post, pk=pk)
@@ -43,3 +43,12 @@ class BlogDetailView(View):
             'post' : post
         }
         return render(request, 'blog_detail.html', context)
+    
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields=['title', 'content']
+    template_name = "blog_update.html"
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('blog:detail', kwargs={'pk':pk})
